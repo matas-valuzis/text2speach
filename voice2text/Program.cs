f8faa235cc80256ee72766cfe7fa1d01f605438b
Matas Valužis <matas.valuzis@gmail.com>
2017-12-15 15:47:36 +0200

    init

---
+++ voice2text/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Threading.Tasks;
+
+
+namespace voice2text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberChoices = new Choices();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                numberChoices.Add(i.ToString());
+            }
+            var gb = new GrammarBuilder();
+            gb.Append("Select player");
+            gb.Append(new SemanticResultKey("number", numberChoices));
+            
+            var sr = new SpeechRecognitionEngine();
+            sr.SetInputToDefaultAudioDevice();
+            sr.LoadGrammar(new Grammar(gb));
+            sr.SpeechRecognized += SpeechRecognized;
+            sr.SpeechDetected += SpeechDetected;
+            sr.SpeechRecognitionRejected += SpeechRejected;
+            // loop recognition
+            sr.RecognizeCompleted += (s, e) => sr.RecognizeAsync();
+
+            Console.WriteLine("Say phrase \"Select player {0-100}\"");
+
+            sr.RecognizeAsync();
+            Console.Read();
+            sr.RecognizeAsyncCancel();
+            
+        }
+
+        private static void SpeechRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            Console.WriteLine("Command not recognized");
+        }
+
+        private static void SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            Console.WriteLine("...");
+        }
+
+        static void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            var res = e.Result.Semantics["number"].Value; 
+            Console.WriteLine("Selected player #{0}", res);
+            
+        }
+
+ 
+
+    }
+}
